5ee59c1e8db8bc2f62d9e87125fe1e63c32294f0
Alex Whinfield <alexwhin@gmail.com>
2022-05-01 11:11:47 +0100

    feat: various design alterations

---
+++ src/components/Logo.svelte
@@ -0,0 +1,9 @@
+<a href="https://whinfield.ltd" rel="noopener noreferrer">
+  <img
+    src="logo-black.png"
+    alt="Whinfield LTD"
+    width="250"
+    height="204"
+    class="w-[175px] h-[143px] lg:w-[250px] lg:h-[204px]"
+  />
+</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,34 +7,35 @@
   import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
   import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
 
+  import Logo from "../components/Logo.svelte";
   import Button from "../components/Button.svelte";
 
-  const company = "Whinfield LTD";
+  const title = "Whinfield LTD";
+  const description = "Alex Whinfield - Full Stack Software Development";
 </script>
 
 <MetaTags
-  title={`Alex Whinfield - Full Stack Software Development | ${company}`}
+  title={`Alex Whinfield - Full Stack Software Development | ${title}`}
   canonical="https://whinfield.ltd/"
   openGraph={{
     url: "https://whinfield.ltd/",
-    title: company,
+    title,
+    description,
     images: [
       {
         url: "https://www.whinfield.ltd/logo-black.png",
         width: 675,
         height: 552,
-        alt: company
+        alt: title
       }
     ],
-    site_name: company
+    site_name: title
   }}
 />
 
 <div class="max-w-md m-auto text-center space-y-6 px-6 py-8">
   <h1 class="flex justify-center">
-    <a href="https://whinfield.ltd">
-      <img src="logo-black.png" alt={company} class="w-[175px] lg:w-[250px]" />
-    </a>
+    <Logo />
   </h1>
   <h2 class="text-2xl opacity-60 flex flex-col">
     <span>Alex Whinfield</span>
@@ -50,13 +51,13 @@
   </div>
   <div class="opacity-40 text-sm space-y-2">
     <p>
-      <span class="uppercase">{company}</span> is a company registered in England and Wales owned
-      and operated by <span class="uppercase">Alex Whinfield</span> &copy; {new Date().getFullYear()}
+      {title} is a company registered in England and Wales, owned and operated by Alex Whinfield &copy;
+      {new Date().getFullYear()}
     </p>
     <p class="flex flex-col space-x-2 justify-center space-y-1 lg:flex-row lg:space-y-0">
       <span>Company Number TBC00000</span>
       <a
-        class="pt-1 opacity-70 hidden lg:block"
+        class="pt-1 opacity-60 hidden lg:block"
         href="https://github.com/alexwhin/whinfield.ltd"
         rel="noopener noreferrer"
         target="_blank"
